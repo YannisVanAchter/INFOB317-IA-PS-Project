e8ef876bc763530bd5387a79af175e4fc7096052
Maragaux <100001703+Maragaux@users.noreply.github.com>
2024-04-14 12:58:06 +0200

    questions bot

--- Bot/bot_TDF.pl
+++ Bot/bot_TDF.pl
@@ -3,15 +3,121 @@
 % Reponse est une liste de mots
 % le cas de base est pour le moment de dire fin au bot
 
+% il faudra une liste des questions qu'un utilisateur peut poser, donc voir les règles
+
+
     %  cas de base
-    produire_reponse([fin],Reponse):-
-        Reponse=[fin,de,la,conversation].
-    
-        % pour produire une réponse, il faut faut
-        % -trouver le mot clé de la question
-        % appeler une règle qui correspond à ce mot clé
-        % 
-    produire_reponse(Question,Reponse):-.
+produire_reponse([fin],Reponse):-
+    Reponse=[fin,de,la,conversation].
+
+    % pour produire une réponse, il faut faut
+    % -trouver le mot clé de la question
+    % -appeler une règle qui correspond à ce mot clé
+    % je comprends pas pourquoi le match pattenr de Jacquet? Voir si la question correspond à un pattern, si on a un mot clé, pourquoi?
+    % peut-être que ce sera utile dans le cas où on a deux questions un peu similaire, à voir, mais pour ça il faudra la liste des questions
+
+produire_reponse(Question,Reponse):-
+    mot_cle(Mot),member(Mot,Question),
+    reponse(Mot,Reponse).
+
+
+mot_cle(commence).
+
+reponse(commence,Reponse):-
+    Reponse=[ "c'est", au, joueur, ayant, la, plus, haute, carte, secondes, de ,"commencer." ].
+
+mot_cle(equipe).
+
+reponse(equipe,Reponse):-
+    Reponse=[ chaque, equipe, compte, X, "coureurs." ],nb_coureurs(X).
+
+nb_coureurs(3).
+nb_equipes(4).
+
+mot_cle(but).
+
+reponse(but,Reponse):-
+    Reponse= [le, but, du, jeu, est, "d'obtenir", le, meilleur, score, possible].
+
+mot_cle(jaune).
+
+reponse(jaune,Reponse):-
+    Reponse=[le, maillot, jaune, appartient, au, coureur, le, plus, rapide].
+
+mot_cle(vainqueur).
+
+reponse(vainqueur,Reponse):-
+    Reponse=[le, vainqueur, est, le, joueur, qui, "possède", le, meilleur, temps, et, qui, a, le, plus, de, points, au, classement].
+
+mot_cle(cartes).
+
+reponse(cartes,Reponse):-
+    Reponse=[chaque, joueur, "reçoit", 5, cartes, par, coureur, en, "début", de, partie, ",", puis, 5, cartes, par, coureur, quand, elles, sont, toutes,"épuisées"].
+
+% l'accent de déplacer, c'est un peu compliqué pour prolog...
+mot_cle(deplacer).
+
+reponse(deplacer,Reponse):-
+    Reponse=[un, coureur, peut, "être", "déplacé", tout, droit, ou, en, diagonale, mais, pas, sur, le, "côté", ou, en, "arrière",".",le, coureur, en ,"tête", est, "déplacé", en, premier,",",suivi, du, coureur, 2, puis, 3, etc,"."].
+
+mot_cle(chute).
+
+reponse(chute,Reponse):-
+    Reponse=[une, chute,en,"série",a,lieu,"lorsqu'un",coureur,tente,de,passer,sur,la,case,"d'un",autre,coureur,ou,de,"s'y","arrêter",".",le,chute,a,alors,lieu,sur,toute,la,largeur,de,la,route,et,implique,le,coureur,ayant,"provoqué",la,chute,",",le,coureur,avec,lequel,il,est,"entré",en,contact,",",tous,les,coureurs,se,trouvant,sur,une,case,adjacente,portant,le,"même","numéro",et,tous,les,coureurs,arrivant,sur,une,case,"affectée",lors,de,ce,tour,".",si,les,deux,voies,"d'une",route,sont,"séparées",par,un,"terre-plein","l'autre","côté",de,la,route,"n'est",alors,pas,"affectéé,",".","lorsqu'une",chute,en,"série",a,lieu,on,place,tous,les,coureurs,"concernés",sur,le,bord,de,la,route,dans,"l'ordre",de,"l'accident",du,coureur,ayant,"provoqué",la,chute,au,coureur,"arrivé",en,dernier,".",les,coureurs,perdent,alors,1,carte,seconde,et,repartiront,dans,"l'ordre",dans,lesquel,ils,sont,"placés",sur,le,bord,de,la,route].
+
+mot_cle(priorite).
+
+reponse(priorite,Reponse):-
+    Reponse=[un, coureur, "situé",sur,une,case,"numérotée",a,la,"priorité",et,passe,avant,les,autres,coureurs].
+
+mot_cle(aspiration).
+
+reponse(aspiration,Reponse):-
+    Reponse=[si,un,coureur,est,"situé",directement,"derrière",un,autre,coureur,ou,un,peloton,",",ce,coureur,peut,gagner,une,seconde,"supllémentaire",sur,sa,carte,seconde,si,cela,lui,permet,de,se,positioner,"derrière",ou,"à","côté",de,"l'autre",coureur,".",le,coureur,de,"tête",ne,peut,pas,profiter,de,ce,"phénomène",".",les,aspirations,ne,sont,pas,obligatoires].
+
+mot_cle(echange).
+
+reponse(echange,Reponse):-
+    Reponse=[une,case,"échange",permet,de,"défauser",3,cartes,et,"d'en",repriocher,3,autres,si,le,coureur,"s'arrête",sur,cette,case,".",si,le,joueur,"possède",moins,de,3,cartes,",","celui-ci",les,"défausse",toutes,et,repioche,le,"même",nombre,de,cartes,que,celui,"qu'il","possdédait",ensuite].
+
+mot_cle(chance).
+
+reponse(chance,Reponse):-
+    Reponse=[une,carte,chance,doit,"être","utilisée","imméditamment","après",avoir,"été","piochée",",",sauf,mention,contraire,".",une,carte,chance,ne,provoque,pas,de,chute,en,"série",si,elle,cause,un,"déplacement",mais,elle,peut,conduire,vers,une,case,"où",une,chute,a,eu,lieu,",",ou,peut,avoir,pour, but,"d'en",causer,une].
+
+mot_cle(sprint).
+
+reponse(sprint,Reponse):-
+    Reponse=[lors,"d'un",sprint,1,ou,plusieurs,coureurs,peuvent,gagner,des,secondes,qui,seront,"décomptées",du,temps,final,du,coureur].
+
+mot_cle(montees).
+
+reponse(montees,Reponse):-
+    Reponse=[les,"montées",sont,"indiquées",par,des,"flèches",rouges,sur,la,carte,".",elles,ralentissent,les,coureurs,de,"moitié",".",un,coureur,en,"montée",ne,peut,pas,profiter,"d'une",apsiration].
+
+mot_cle(descente).
+
+reponse(descente,Reponse):-
+    Reponse=[les,descentes,sont,"indiquées",par,des,"flèches",bleues,sur,la,carte,".",dans,une,descente,un,coureur,gagne,2,secondes,et,non,une,lors,"d'un",apsiration,et,peut,alors,"dépasser",le,coureur,devant,lui].
+
+mot_cle(depassement).
+
+reponse(depassement,Reponse):-
+    Reponse=[un,coureur,qui,choisi,"d'avancer",lentement,doit,prendre,en,compte,les,coureurs,qui,le,suivent,afin,"d'éviter",une,chute,".",il,doit,alors,laisser,la,"possibilité",au,coureur,qui,le,suit,"d'utiliser",une,carte,4,secondes,ou,6,en,"montée",sauf,si,le,coureur,"derrière",lui,peut,le,doubler,normalement,".",les,coureurs,suivants,"n'ont",alors,le,droit,de,provoquer,une,chute,que,si,cette,"dernière",est,"inévitable"].
+
+mot_cle(fin).
+
+reponse(fin,Reponse):-
+    Reponse=[le,coureur,qui,termine,en,premier,le,plus,loin,"derrière",la,ligne,"d'arrivée",obtient,le,meilleur,score,en,terme,de,temps,et,gagne,"l'étape",".",une,apsiration,ne,peut,plus,avoir,lieu,lorsque,la,ligne,"d'arrivée",est,"passée",tout,comme,une,chute,en,"série",".",si,plusieurs,coureurs,obtiennent,le,"même",score,celui,"situé",sur,la,case,"numérotée","l'emporte",".",un,coureur,passant,la,ligne,"d'arrivée","après","d'autres",coureurs,et,obtenant,un,meilleur,score,ne,"l'emporte",pas,car,il,est,"arrivé","à",un,tour,"ultérieur",".",pour,ce,coureur,on,ajoute,donc,10,secondes,"supplémentaires","à",son,score,"après",chaque,tour].
+
+mot_cle(gagne).
+
+reponse(gagne,Reponse):-
+    Reponse=[le,coureur,qui,arrive,en,premier,le,plus,loin,"derrière",la,ligne,"d'arrivée",remporte,"l'étape"].
+
+
+
+
     
     
     % -----------------------------------------------------------------------
@@ -297,4 +403,4 @@
     /*                                                                       */
     /* --------------------------------------------------------------------- */
     
-    :- tourdefrance.
+    % :- tourdefrance.
