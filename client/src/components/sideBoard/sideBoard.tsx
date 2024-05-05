4460234ad67030c91a92018285da3b12cce339a4
Yannis Van Achter <91072987+YannisVanAchter@users.noreply.github.com>
2024-05-05 20:54:00 +0200

    Update sideBoard.tsx
    Winner ranking instead of player position

--- client/src/components/sideBoard/sideBoard.tsx
+++ client/src/components/sideBoard/sideBoard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { DCtx } from '../../Game';
+import { DCtx, Ctx } from '../../Game';
+import { winnerRanking } from '../../Game';
 
 import './sideBoard.css';
 
 type TODO = {
     G: DCtx,
-    ctx: any,
+    ctx: Ctx,
     moves: any,
     events: any,
     playerID: any,
@@ -17,25 +18,21 @@ type TODO = {
     gameMetadata: any,
     matchData: any,
     isPreview: any,
+    className: string,
 };
 
 function SideBoard(props: TODO) {
+    const winners = winnerRanking({G: props.G, ctx: props.ctx});
     return (
-        <aside>
-            {props.G.players.map((player, i) => {
-                return (
-                    <div key={i} className='player'>
-                        <h3>Player: {player.playerID + 1}</h3>
-                        {player.bikes.map((bike, j) => {
-                            return (
-                                <div key={j}>
-                                    <p>Bike: {bike.position}c</p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                );
-            })}
+        <aside className={`${props.className} winner`}>
+            <h2>Winner</h2>
+            <ol>
+                {winners.map((player) => (
+                    <li key={player} className='player'>
+                        <span>Player {player}</span>
+                    </li>
+                ))}
+            </ol>
         </aside>
     );
 }
