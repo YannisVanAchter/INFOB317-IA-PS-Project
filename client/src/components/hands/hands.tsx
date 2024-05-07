1f6185b06bc0a8eb56b1fdbc4583ff6d72e6560a
Yannis Van Achter <91072987+YannisVanAchter@users.noreply.github.com>
2024-05-07 23:38:58 +0200

    Responsive card display

--- client/src/components/hands/hands.tsx
+++ client/src/components/hands/hands.tsx
@@ -1,54 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import CardFront from '../../assets/cardFront';
 import { DCtx, Ctx } from '../../Game';
-import { useCardOnBike } from '../../Game';
+import { mockUseCardOnBike } from '../../Game'; 
+import { deepCopy } from '../../utils/deep_copy';
 
 import './hands.css';
 
 type TODO = {
     G: DCtx,
     ctx: Ctx,
-    copiedG: DCtx,
 };
 
 // TODO: finish this component once mock data for simulation is available
 function DisplayHands(props: TODO) {
-    let { G, ctx, copiedG } = props;
+    let { G, ctx } = props;
     const currentPlayer = ctx.currentPlayer;
     let players = G.players;
-    // const [hoveredCardIndex, setHoveredCardIndex] = useState<number | null>(null);
-    // const [clickedCardIndex, setClickedCardIndex] = useState<number | null>(null);
 
-    // const copiedGTemp = useCardOnBike({G: copiedG, ctx}, hoveredCardIndex !== null ? hoveredCardIndex : 0);
-    // const clickedGTemp = useCardOnBike({G: G, ctx}, clickedCardIndex !== null ? clickedCardIndex : 0);
-
-    // const handleMouseOver = (card: number, index: number) => {
-    //     console.log('Card:', card, 'Index:', index);
-    //     setHoveredCardIndex(index);
-    // }
-    // const handleClickedCard = (card: number, index: number) => {
-    //     console.log('Apply useCardOnBike\nCard:', card, 'Index:', index);
-    //     setClickedCardIndex(index);
-    // }
-    // if (copiedGTemp !== null) {
-    //     copiedG = copiedGTemp;
-    // }
-    // if (clickedGTemp !== null) {
-    //     G = clickedGTemp;
-    // }
+    // TODO: add modal to display choices for player to make when card is played and has multiple destinations
 
     return (
         <div className='hands'>
             {players.map((player, i) => {
+                // if (i.toString() !== currentPlayer) return;
+                let hand = deepCopy(player.hand);
+                while (hand.length < 5) {
+                    console.log(hand);
+                    if (hand.length % 2 === 1) { // if even
+                        hand = [-1, ...hand, -1];
+                    }
+                    else {
+                        const middle = parseInt(`${hand.length / 2}`);
+                        const left = hand.slice(0, middle);
+                        const right = hand.slice(middle);
+                        hand = [...left, -1, ...right];
+                    }
+                }
                 return (
                     <div key={i} className={`player ${currentPlayer === player.toString() ? 'current': ''}`}>
                         <h3>Player: {player.playerID + 1}</h3>
                         <ul className='cards'> 
-                            {player.hand.map((card, j) => {
+                            {hand.map((card, j) => {
+                                if (card === -1) return (
+                                    <li
+                                        key={j}
+                                        className={`card card-${j} empty-card`}
+                                    >
+                                        <CardFront number={-1} />
+                                    </li>
+                                );
                                 return (
                                     <li
-                                        key={j} 
+                                        key={j}
                                         className={`card card-${j}`}
                                     >
                                         <CardFront number={card} />
