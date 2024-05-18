56b39f77889317273f23e9107db6793b33c31f83
Caribou26 <127028027+Loup-K@users.noreply.github.com>
2024-05-19 01:44:34 +0200

    update socket.pl
    toujours des soucis avec les requetes, je vais nettoyer le code après

--- server/socket.pl
+++ server/socket.pl
@@ -43,35 +43,59 @@ openfile_json(File) :- %predicat pour ouvrir un fichier json (uniquement pour le
     open(File, read, Stream),
     json_read_dict(Stream, Dict),
     close(Stream),
-    answer_ia(Dict, _).
+    answer_ia(Dict, 2).
 
 extract_json(Request) :- %predicat pour extraire le json de la requête
     http_read_json_dict(Request, Dict), 
-    answer_ia(Dict, _Request). %on appelle le prédicat pour l'ia
-        
+    answer_ia(Dict, 1). %on appelle le prédicat pour l'ia
+    
 answer_ia(Board, _Request) :- %predicat pour l'ia
-    %write("Board: "), writeln(Board), %pour tester
     extract_board(Board, BoardData), %on tranforme le format des données pour que l'ia puisse les utiliser
+    write("BoardData: "), writeln(BoardData),
     get_move_IA(BoardData, Move), %A changer pour le predicat qui renvoie la réponse de l'ia
     cors_enable,
-    reply_json(json([response=Move])). %on renvoie la réponse de l'ia
+    write("Move: "), writeln(Move),
+    %reply_json(json([response=Move])). %on renvoie la réponse de l'ia
+    format_move(Move, NewMove),
+    move_to_char(NewMove, Response), %on crée le json à partir de la réponse de l'ia
+    write("Response: "), writeln(Response), %pour tester
+    reply_json(json([response=Response])). %on renvoie le json
 
-%get_move_IA(Board, Move) :- %uniquement pour tester en attendant l'ia, à supprimer après
-%    write("Board: "), writeln(Board), 
+format_move(Move, NewMove) :- %predicat pour tester
+    Move =.. [_|NewMove],
+    write("NewMove: "), writeln(NewMove).
+
+move_to_char(Move, String) :- %predicat pour transformer le move en String
+    Move = [Number, (String1, String2)],
+    number_chars(Number, NumList), atom_chars(NumChar, NumList),
+    List = [NumChar, String1, String2],
+    atomic_list_concat(List, ', ', String),
+    write("String: "), writeln(String).
+
+get_move_IA2(Board, Move) :- %uniquement pour tester en attendant l'ia, à supprimer après
+    write("Board: "), writeln(Board), 
     %MoveResponse = "ROARRRRRRRRRRRRRRRRR", %pour tester une valeur de retour pour move
-%    Move = Board.
+    Move = Board.
 
-extract_board(Board, BoardData) :- %Board est un dict contenant le contenu du JSON et on veut que BoardData soit une liste de liste contenant uniquement les infos nécéssaires pour l'IA
+extract_board(Board, FinalBoard) :- %Board est un dict contenant le contenu du JSON et on veut que BoardData soit une liste de liste contenant uniquement les infos nécéssaires pour l'IA
     Players = Board.players, %On récupère les infos des joueurs
     CurrentPlayerID = Board.currentPlayer.playerID, %On récupère l'ID du joueur actuel
     reorder_players(Players, CurrentPlayerID, ReorderedPlayers), %On réorganise les joueurs pour que le joueur actuel soit le premier
-    maplist(extract_player, ReorderedPlayers, BoardData). %on applique le prédicat extract_player à chaque joueur pour récupérer les infos nécéssaires pour l'IA
+    maplist(extract_player, ReorderedPlayers, NewBoard), %on applique le prédicat extract_player à chaque joueur pour récupérer les infos nécéssaires pour l'IA
+    change_ID(NewBoard, 0, [], BoardData),
+    reverse(BoardData, FinalBoard).
+
+change_ID([], _, BoardData, BoardData).
+change_ID([[_, Hand, Position]|OtherPlayer], I, Acc, BoardData) :- 
+    NewID = I,
+    I1 is I + 1,
+    change_ID(OtherPlayer, I1, [[NewID, Hand, Position]|Acc], BoardData).
 
 reorder_players(Players, CurrentPlayerID, ReorderedPlayers) :- %prédicat pour réorganiser les joueurs
-    partition(current_player_ge(CurrentPlayerID), Players, CurrentAndGreater, LessThanCurrent), %On sépare les joueurs avec des ids > ou = à celui du joueur actuel des autres
+    partition(current_player(CurrentPlayerID), Players, CurrentAndGreater, LessThanCurrent), %On sépare les joueurs avec des ids > ou = à celui du joueur actuel des autres
     append(CurrentAndGreater, LessThanCurrent, ReorderedPlayers). %On met le joueur actuel en premier dans la liste 
-    
-current_player_ge(CurrentPlayerID, Player) :- %prédicat pour vérifier si un joueur a un id supérieur ou égal à celui du joueur actuel
+
+current_player(CurrentPlayerID, Player) :- %prédicat pour vérifier si un joueur a un id supérieur ou égal à celui du joueur actuel
     Player.playerID >= CurrentPlayerID.
 
 extract_player(Player, PlayerData) :- %Player est un dict contenant les infos d'un joueur 
@@ -79,7 +103,8 @@ extract_player(Player, PlayerData) :- %Player est un dict contenant les infos d'
     Hand = Player.hand, %On récupère la main du joueur
     Bikes = Player.bikes, %On récupère les infos des vélos du joueur
     maplist(bike_position, Bikes, BikePositions), %On applique le prédicat bike_position à chaque vélo pour récupérer leur position
-    PlayerData = [PlayerID, Hand, BikePositions]. %On crée une liste contenant l'ID du joueur, sa main et les positions de ses vélos
-    
-bike_position(Bike, Position) :- %Bike est un dict contenant les infos d'un vélo
+    maplist(string_to_atom, BikePositions, BikePositionsChar), %pour le fichier
+    PlayerData = [PlayerID, Hand, BikePositionsChar]. %On crée une liste contenant l'ID du joueur, sa main et les positions de ses vélos
+
+    bike_position(Bike, Position) :- %Bike est un dict contenant les infos d'un vélo
     Position = Bike.position. %On récupère la position du vélo
--- server/test_request.py
+++ server/test_request.py
@@ -13,36 +13,36 @@ data = {
             "playerID": 0,
             "hand": [1, 4, 2, 9, 6],
             "bikes": [
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0}
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0}
             ]
         },
         {
             "playerID": 1,
             "hand": [12, 9, 7, 11, 9],
             "bikes": [
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0}
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0}
             ]
         },
         {
             "playerID": 2,
             "hand": [5, 10, 6, 11, 3],
             "bikes": [
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0}
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0}
             ]
         },
         {
             "playerID": 3,
             "hand": [8, 3, 11, 3, 8],
             "bikes": [
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0},
-                {"position": "3-A-left", "reduce": 0, "turn": 0}
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0},
+                {"position": "3_A_left", "reduce": 0, "turn": 0}
             ]
         }
     ]
